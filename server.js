e71bb90b26bb31011eed1373df4898010ec1abf0
Sungjun <hongsi140626@gmail.com>
2019-01-08 00:22:43 -0500

    v3

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 const path = require('path');
 
-app.use(express.static(path.join(__dirname, './sound/public')));
+app.use(express.static(path.join(__dirname, './sound/src')));
 
 const soundList = [
     {
